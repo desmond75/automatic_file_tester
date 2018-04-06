63d3593d9a5ee25a4a1f928325411861daf007ce
desmond67 <adedesmond6@gmail.com>
2018-04-05 20:00:27 -0700

    i added a way to handle unexpected circumstance

--- program_to_test_musics.py
+++ program_to_test_musics.py
@@ -17,23 +17,29 @@ def automatic_file_tester(full_path_to_file,number_of_files_to_test, test_time_i
 	    # number_of_files_to_test is the amount of files you wish to test for example you may 
 	    want to test the first 20 files it depends on you  
 	    # test_time_in_seconds is the time you want a file to be tested,for example you may want 
-	    each file to be tested for 5 seconds or more it depends on you 
+	    each file to be tested for 5 seconds or more it depends on you
+
 	    """
-	for file in islice(os.listdir(full_path_to_file),number_of_files_to_test):
-		#loops through the files in the path provided
-		absolute_path_to_file = full_path_to_file + file #file name is added to the full_path_to_file
-		time.sleep(test_time_in_seconds) #waiting time during each file testing
-		os.startfile(absolute_path_to_file)#opens file using the default program on your computer
-		print(f'I am now testing --> {file}')
+	try:
+		if not os.path.exists(full_path_to_file):
+			print('Your path is incorrect')
+		else:
+			for file in islice(os.listdir(full_path_to_file),number_of_files_to_test):#loops through all files in the directory
+
+				path = os.path.join(full_path_to_file , file)# attach file with the full path provided
+				time.sleep(test_time_in_seconds) #waiting time during each file testing
+				os.startfile(path)#opens file using the default program on your computer
+				print(f'I am now testing --> {file}')
+
+	except BaseException as e:
+		print('An error occured', e)
 
 
 #To run the program uncomment the variables below and enter your own path to the 
 #files you wish to test
 
 
-
 # full_path_to_file = "D://aud/" #Enter your path
 # number_of_files_to_test = 9 #You can set this to any number you want to
 # test_time_in_seconds = 5 #You can change this to anything you want to make the test time much longer
-
 # automatic_file_tester(full_path_to_file, number_of_files_to_test, test_time_in_seconds)
